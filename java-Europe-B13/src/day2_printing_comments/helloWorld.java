dab7c82c5f01d706dde8bfd06e79348dfeebedf2
Meral <meralmoussa@icloud.com>
2019-10-10 11:25:46 +0200

    First Commit

---
+++ java-Europe-B13/src/day2_printing_comments/helloWorld.java
@@ -0,0 +1,20 @@
+package day2_printing_comments;
+
+public class helloWorld{
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+		System.out.println("APPLE");
+		
+		
+		
+		
+	
+		
+		
+		
+	}
+
+}
