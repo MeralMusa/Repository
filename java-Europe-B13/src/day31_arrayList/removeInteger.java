4f999b5da648f01e843c14c008832631e8f2e898
Musa Meral <meralmoussa@icloud.com>
2019-12-13 03:10:44 +0100

    new commit

--- java-Europe-B13/src/day31_arrayList/removeInteger.java
+++ java-Europe-B13/src/day31_arrayList/removeInteger.java
@@ -5,64 +5,41 @@ import java.util.ArrayList;
 public class removeInteger {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ArrayList<String> cities = new ArrayList<>();
-		
-		cities.add("London");
-		cities.add("Virginia");
-		cities.add("DC");
-		cities.add("Baku");
-		cities.add("Berlin");
-		cities.add("Baku");
-		
-		System.out.println(cities.toString());
-		
-		//Print each city by using for-each loop seperated by space
-		
-		for(String city : cities) {
-			System.out.println(city + " ");
-		}
-		
-		System.out.println("----------------------------");
-		
-		//Print each city by using for-iterator loop
-		
-		for(int i=0;i<cities.size();i++) {
-			System.out.println(cities.get(i));
-		}
-		
-		System.out.println("----------------------------");
-		
-		System.out.println("Removing Baku");
-		cities.remove("Baku"); 
-		
-		cities.remove("New York");
-		
-		System.out.println(cities.toString());
-		
-		System.out.println("----------------------------");
-		
-		cities.add(0, "Arlington");
-		cities.add(1,"Istanbul");
-		
-		System.out.println(cities.toString());
-		
-		cities.add(2, "Ankara");
-		
-		System.out.println(cities.toString());
-		
-		System.out.println("----------------------------");
-		
-		int idx = cities.indexOf("Istanbul");
-		System.out.println(idx);
-		
-		System.out.println(cities.indexOf("Bursa"));
-		
-		System.out.println("----------------------------");
-		
-		cities.clear();
-		boolean empty = cities.isEmpty();
-		System.out.println(empty);
-	}
+			
+			ArrayList<Integer> nums = new ArrayList<>();
+			
+			System.out.println(nums.size());
+			System.out.println(nums.isEmpty());
+			System.out.println(nums.size()==0);
+			
+			nums.add(4);
+			nums.add(1);
+			nums.add(44);
+			nums.add(5);
+			nums.add(10);
+			nums.add(100);
+			nums.add(55);
+			
+			System.out.println(nums.toString());
+			
+			nums.remove(5); //removes the spficied index number from the list
+			
 
-}
+			
+			System.out.println(nums.toString());
+			
+			Integer n1 = new Integer(5); //Converting primitive to object
+			Integer n2 = Integer.valueOf(5);
+			
+			nums.remove(n1);  //removes the specified element from the list
+			
+			System.out.println(nums.toString());
+			
+			nums.remove(new Integer(4));  //removes the specified element from the list
+			
+			System.out.println(nums.toString());
+			
+			
+		}
+
+	}
